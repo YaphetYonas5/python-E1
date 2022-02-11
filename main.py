85f29d01bcd6ca0b158ac67beb6196596eccf468
YaphetYonas1 <85519133+YaphetYonas5@users.noreply.github.com>
2022-02-11 03:43:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+name = input("what is your name :")
+age = int(input("hello " + name + " how old are you :"))
+year = int(input("what year is it:"))
+print("you will be 100 in...")
+print(100 - age + year )
+num = int(input("tell me a number :"))
+age1 = (100 - age + year )
+year2 = (100 * num + year - age)
+print(year2)
+print("is the year you will be")
+age2 = (100 * num)
+print(age2)
+print("why are you telling me this ")
+print(2 * "IDK ")
+
+
+
